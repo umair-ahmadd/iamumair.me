774779a819aed4f59c14b4ee8904fdc3133013ae
Umair Ahmad <3.umairahmad@gmail.com>
2026-07-21 23:49:10 +0430

    add database logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ server.listen(3000, function () {
   console.log("Server is listening on port 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    console.log("Server closed.");
+    shutdownDB();
+  });
+});
+
 /** begin websocket */
 const WebSocketServer = require("ws").Server;
 
@@ -26,6 +36,10 @@ wss.on("connection", function connection(ws) {
     ws.send(`Welcome to my server!`);
   }
 
+  db.run(
+    `INSERT INTO visitors (count, time) VALUES (${numClients}, datetime("now"))`,
+  );
+
   ws.on("close", function close() {
     console.log("A client has disconnected.");
     wss.broadcast(`Current visitors: ${numClients}`);
@@ -37,3 +51,29 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** end websockets */
+/** begin database   */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+    count INTEGER,
+    time TEXT
+    )
+    `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down database...");
+  db.close();
+}
